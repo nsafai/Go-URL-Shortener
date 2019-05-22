4876db91c419c1e3d820fc5772c6791fb5ae0ea3
Nicolai Safai <nicolaisafai@gmail.com>
2019-05-21 19:39:10 -0700

    added some scaffolding to return all shortlinks

--- handler.go
+++ handler.go
@@ -2,7 +2,9 @@ package urlshort
 
 import (
 	"net/http"
-	yaml "gopkg.in/yaml.v2"
+	// yaml "gopkg.in/yaml.v2"
+	"encoding/json"
+	"fmt"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -26,6 +28,7 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
+/*********** YAML ***********/
 // YAMLHandler will parse the provided YAML and then return
 // an http.HandlerFunc (which also implements http.Handler)
 // that will attempt to map any paths to their corresponding
@@ -42,19 +45,28 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// 1. Parse the YAML
-	pathUrls, err := parseYaml(yamlBytes)
-	if err != nil {
-		return nil, err
-	}
-	// 2. Convert YAML array into map
-	pathsToUrls := buildMap(pathUrls)
-	// 3. return a map handler using the map
-	return MapHandler(pathsToUrls, fallback), nil // returning nil as error because we're confident not to have an error here
-}
+// func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+// 	// 1. Parse the YAML
+// 	pathUrls, err := parseYaml(yamlBytes)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	// 2. Convert YAML array into map
+// 	pathsToUrls := buildMap(pathUrls)
+// 	// 3. return a map handler using the map
+// 	return MapHandler(pathsToUrls, fallback), nil // returning nil as error because we're confident not to have an error here
+// }
 
-func buildMap(pathUrls []pathUrl) map[string]string {
+// func parseYaml(data []byte) ([]PathUrl, error) {
+// 	var pathUrls []PathUrl
+// 	err := yaml.Unmarshal(data, &pathUrls)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return pathUrls, nil
+// }
+
+func buildMap(pathUrls []PathUrl) map[string]string {
 	pathsToUrls:= make(map[string]string)
 	for _, pu := range pathUrls {
 		pathsToUrls[pu.Path] = pu.URL
@@ -62,16 +74,32 @@ func buildMap(pathUrls []pathUrl) map[string]string {
 	return pathsToUrls
 }
 
-func parseYaml(data []byte) ([]pathUrl, error) {
-	var pathUrls []pathUrl
-	err := yaml.Unmarshal(data, &pathUrls)
+/*********** JSON ***********/
+
+func JSONHandler(jsonBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// 1. Parse the JSON
+	pathUrls, err := ParseJson(jsonBytes)
 	if err != nil {
 		return nil, err
 	}
+	// 2. Convert JSON array into map
+	pathsToUrls := buildMap(pathUrls)
+	// 3. return a map handler using the map
+	return MapHandler(pathsToUrls, fallback), nil // returning nil as error because we're confident not to have an error here
+}
+
+func ParseJson(data []byte) ([]PathUrl, error) {
+	var pathUrls []PathUrl
+	err := json.Unmarshal(data, &pathUrls)
+	if err != nil {
+		fmt.Println("error parsing json!")
+		return nil, err
+	}
+	// pu := PathUrl{"/test", "http://google.com"}
 	return pathUrls, nil
 }
 
-type pathUrl struct {
+type PathUrl struct {
 	Path string `yaml:"path"`
 	URL string `yaml:"url"`
 }
--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	// "encoding/json"
 	".."
 )
 
@@ -16,28 +17,55 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+// 	// Build the YAMLHandler using the mapHandler as the
+// 	// fallback
+// 	yaml := `
+// - path: /urlshort
+//   url: https://github.com/gophercises/urlshort
+// - path: /urlshort-final
+//   url: https://github.com/gophercises/urlshort/tree/solution
+// `
+// 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+	jsonLinks := `[{
+		"path":"/cool",
+		"url":"http://cool.com"
+}, {
+		"path":"/new",
+		"url":"http://new.com"
+}]`
+
+	jsonHandler, err := urlshort.JSONHandler([]byte(jsonLinks), mapHandler)
 	if err != nil {
 		panic(err)
 	}
+	
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", jsonHandler)
+
 }
 
 func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/links", allLinks)
 	mux.HandleFunc("/", hello)
 	return mux
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to the URL Shortener API. For more info, visit https://github.com/nsafai/Go-URL-Shortener")
+}
+
+func allLinks(w http.ResponseWriter, r * http.Request) {
+	jsonLinks := `[{
+		"path":"/cool",
+		"url":"http://cool.com"
+}, {
+		"path":"/new",
+		"url":"http://new.com"
+}]`
+	fmt.Fprintln(w, jsonLinks)
 }
